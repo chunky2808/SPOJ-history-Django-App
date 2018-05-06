1715ed3bd7e229b8fa6e03f73dfd88e87b2ce019
paras2808 <parasjain2896@gmail.com>
2018-05-07 01:29:40 +0530

    Link clickable from site accepting username

--- crawl/models.py
+++ crawl/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class paras(models.Model):
-	name = models.CharField(max_length = 40, unique =True)
+	name = models.CharField(max_length = 140)
 	def __str__(self):
 		return self.name
+
+class jain(models.Model):
+	hits = models.IntegerField()		
 # Create your models here.
--- crawl/views.py
+++ crawl/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .Spoj_with_multithreading import main
 from .forms import NewTopicForm
-from .Spoj_with_multithreading import main
+from .models import jain
 
 def home(request):
+	hit = jain.objects.filter(hit)
+	for hit in hit:
+		hits = hit.hits
+		print(hits)
+	print(hits)
 	if request.method == 'POST':
 		form = NewTopicForm(request.POST)
 		if form.is_valid():
@@ -20,6 +25,6 @@ def home(request):
 			return render(request, 'list_question.html', {'list': l})
 	else:
 		form = NewTopicForm()
-	return render(request, 'create.html', {'form': form})
+	return render(request, 'create.html', {'form': form,'hits':hits})
 
 	
